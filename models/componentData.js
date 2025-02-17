59feb27c1b7802177399a36e74763d00e327a08b
Tu Nombre <tuemail@example.com>
2025-02-17 07:45:43 -0600

    feat: historialComponentes

--- models/componentData.js
+++ models/componentData.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 
 const ComponentDataSchema = new mongoose.Schema({
-  componenteId: { type: mongoose.Schema.Types.ObjectId, ref: "Componentes", required: true },
+  componenteId: { type: mongoose.Schema.Types.ObjectId, ref: "Componente", required: true }, // ✅ Aquí está corregido
   estado: Boolean,
   fecha: { type: Date, default: Date.now }
 });
